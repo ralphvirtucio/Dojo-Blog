ff0ec8d0e3a3aa4bbba01ff013d90fad31e8a1d2
ralphvirtucio <ralphvirtucio3@gmail.com>
2021-01-08 03:56:48 +0800

    refactor useeffect dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,6 +23,8 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = id => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -30,8 +32,8 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect run');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
@@ -40,6 +42,8 @@ const Home = () => {
         blogs={blogs.filter(blog => blog.author === 'mario')}
         title="Mario Blogs!"
       /> */}
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
