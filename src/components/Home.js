37d92a2aa96ee5b900a8ba3891a60697d1874713
ralphvirtucio <ralphvirtucio3@gmail.com>
2021-01-13 06:39:33 +0800

    refactor-reuse-hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useFetch from '../hooks/useFetch';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  // const handleDelete = id => {
-  //   const newBlogs = blogs.filter(blog => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
-
-  useEffect(() => {
-    fetch(`http://localhost:8000/blogs`)
-      .then(res => {
-        if (!res.ok) {
-          throw Error('ERROR FETCH!');
-        }
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch(error => {
-        setIsPending(false);
-        setError(error.message);
-      });
-  }, []);
+  const [data, isPending, error] = useFetch(`http://localhost:8000/blogs`);
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
       {isPending && <div>Loading</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {data && <BlogList blogs={data} title="All Blogs!" />}
     </div>
   );
 };
